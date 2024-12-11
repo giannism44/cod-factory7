82bd3ce1e8a91312e3f27c76b9ed6be95eeedf17
Ioannis Michos <giannismichos004@gmail.com>
2024-12-11 10:59:00 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello Coding Factory in
+ * standard output (console)
+ *
+ * @author giann
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Prints Hello Coding Factory
+
+        /*
+         *Print =s in standard output
+         * "Hello Coding Factory"
+         */
+        System.out.println("Hello Coding Factory!"); //Prints Hello Coding Factory
+        System.out.print("Hello World");
+        System.out.println();
+    }
+}
